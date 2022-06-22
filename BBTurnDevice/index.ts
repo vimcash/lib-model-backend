cb73bbf6c4057741e36723b61b72426a295b599c
Raian M. Gil del Rosario <Raian.gdr@gmail.com>
2022-06-22 10:04:56 -0400

    Arreglos Pudrete

--- BBTurnDevice/index.ts
+++ BBTurnDevice/index.ts
@@ -1,5 +1,5 @@
 import * as mongoose from "mongoose";
-import { BBTurnDeviceTable } from "vcl-interface"
+import { BBTurnDeviceTable } from "vcl-interface";
 import { Schema } from './schema';
 
 export interface BBTurnDeviceDocument extends mongoose.Document, BBTurnDeviceTable { }

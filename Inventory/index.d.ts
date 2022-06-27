a2752c20b15aec5834e825cfc1f716aa87f53307
Raian M. Gil del Rosario <Raian.gdr@gmail.com>
2022-06-27 18:31:51 -0400

    arreglando git ignore

--- Inventory/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Document as document, Model as model } from "mongoose";
-import { InventoryTable } from 'vcl-interface';
-export interface InventoryDocument extends document, InventoryTable {
-}
-interface Model extends model<InventoryDocument> {
-    build(atrib: InventoryTable): InventoryDocument;
-}
-export declare const InventoryModel: Model;
-export default InventoryModel;

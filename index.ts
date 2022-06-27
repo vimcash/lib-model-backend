77f6b30682e249dab3a58f9979b1472e464caa0c
Raian M. Gil del Rosario <Raian.gdr@gmail.com>
2022-06-27 16:56:28 -0400

    export InventoryModel

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export { BBTurnDeviceModel } from "./BBTurnDevice"
 export { TradicionalUserModel } from "./TradicionalUser"
 export { ProductModel } from "./Product"
+export { InventoryModel } from "./Inventory"

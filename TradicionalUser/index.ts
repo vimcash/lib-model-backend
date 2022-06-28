95b77384c77b7a5be2d30f05faca60a28ac02d1c
Raian M. Gil del Rosario <Raian.gdr@gmail.com>
2022-06-28 13:59:49 -0400

    fix: Quitando la funcion de checkExist

--- TradicionalUser/index.ts
+++ TradicionalUser/index.ts
@@ -7,20 +7,11 @@ export interface TradicionalUserDocument extends document, TradicionalUserTable 
 
 interface Model extends model<TradicionalUserDocument> {
   build(atrib: TradicionalUserTable): TradicionalUserDocument;
-  existUser(username: string, pass: string): Boolean;
 }
 
 Schema.statics.build = (atrib: TradicionalUserTable) => {
   return new TradicionalUserModel(atrib);
 };
-
-Schema.statics.existUser = async(username: string, pass: string) => {
-  const usuarioExiste = await TradicionalUserModel.findOne({ username });
-  return await Password.compare(
-    usuarioExiste!.password,
-    pass
-  );
-}
 
 export const TradicionalUserModel = mdl<TradicionalUserDocument, Model>(
   'MDVC002T',

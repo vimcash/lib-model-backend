6b6a885ce74b399b7dc135778b982784ae14487b
Raian M. Gil del Rosario <Raian.gdr@gmail.com>
2022-06-28 17:39:39 -0400

    feat: Agregando SimpleCompany Model

---
+++ SimpleCompany/index.ts
@@ -0,0 +1,20 @@
+import { Schema } from './schema';
+import { Document as document, model as mdl, Model as model} from "mongoose";
+import { SimpleCompanyTable } from 'vcl-interface';
+
+export interface SimpleCompanyDocument extends document, SimpleCompanyTable { }
+
+interface Model extends model<SimpleCompanyDocument> {
+  build(atrib: SimpleCompanyTable): SimpleCompanyDocument;
+}
+
+Schema.statics.build = (atrib: SimpleCompanyTable) => {
+  return new SimpleCompanyModel(atrib);
+};
+
+export const SimpleCompanyModel = mdl<SimpleCompanyDocument, Model>(
+  'MDVC002T',
+  Schema
+);
+
+export default SimpleCompanyModel
---
+++ SimpleCompany/schema.ts
@@ -0,0 +1,45 @@
+import { Schema as schema } from 'mongoose';
+
+export const Schema = new schema
+(
+  {
+    companyName: {
+      type: String,
+      uppercase: true,
+      required: true,
+      trim: true
+    },
+    companyDesc: {
+      type: String,
+      lowercase: true,
+      trim: true
+    },
+    createdAt: {
+      type: schema.Types.Date,
+      default: Date.now,
+    },
+    createdBy: {
+      type: String,
+      lowercase: true,
+      trim: true
+    },
+    udpatedAt: {
+      type: schema.Types.Date,
+      default: Date.now,
+    },
+    updatedBy: {
+      type: String,
+      lowercase: true,
+      trim: true
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);

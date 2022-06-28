af8db8603f0cd6b8d941b6fe2db382bef1c37ea5
Raian M. Gil del Rosario <Raian.gdr@gmail.com>
2022-06-28 14:01:26 -0400

    Limpiando el paquete

--- Core/hash.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.hash = void 0;
-var password_1 = require("../services/password");
-var hash = function (pass) { return password_1.Password.toHash(pass); };
-exports.hash = hash;

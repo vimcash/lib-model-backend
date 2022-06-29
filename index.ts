bcd5366f8c92c42746306d99b337598972c10367
Raian M. Gil del Rosario <Raian.gdr@gmail.com>
2022-06-28 20:32:17 -0400

    fix: Agregando a index

--- index.ts
+++ index.ts
@@ -2,5 +2,6 @@ export { BBTurnDeviceModel } from "./BBTurnDevice"
 export { TradicionalUserModel } from "./TradicionalUser"
 export { ProductModel } from "./Product"
 export { InventoryModel } from "./Inventory"
+export { SimpleCompanyModel } from "./SimpleCompany"
 
 export { compare } from "./Core/compare"
